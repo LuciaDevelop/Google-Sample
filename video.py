5f6ff10720c49acba2f79bdd0f730b184a0f9079
LuciaDevelop <88276392+LuciaDevelop@users.noreply.github.com>
2021-08-11 17:11:06 +0100

    Add files via upload

---
+++ video.py
@@ -0,0 +1,59 @@
+"""A video class."""
+
+from typing import Sequence
+
+
+class Video:
+    """A class used to represent a Video."""
+
+    def __init__(self, video_title: str, video_id: str, video_tags: Sequence[str]):
+        """Video constructor."""
+        self._title = video_title
+        self._video_id = video_id
+        self._flag = None
+
+        # Turn the tags into a tuple here so it's unmodifiable,
+        # in case the caller changes the 'video_tags' they passed to us
+        self._tags = tuple(video_tags)
+
+    @property
+    def title(self) -> str:
+        """Returns the title of a video."""
+        return self._title
+
+    @property
+    def video_id(self) -> str:
+        """Returns the video id of a video."""
+        return self._video_id
+
+
+
+    @property
+    def tags(self) -> Sequence[str]:
+        """Returns the list of tags of a video."""
+        return self._tags
+
+@property
+     def flag(self):
+         """Returns a flag reason"""
+         return self._flag
+
+     def set_flag(self, flag_reason: str):
+         """Flags video with a supplied reason"""
+         self._flag = flag_reason
+
+     def allow(self):
+         """Remove flag from video"""
+         self._flag = None
+
+     def __str__(self):
+         return f"{self.title} ({self.video_id}) [{' '.join(self.tags)}]"
+
+     def __eq__(self, other):
+         return self.__str__() == other.__str__()
+
+     def __lt__(self, other):
+         return self.__str__() < other.__str__()
+
+     def __gt__(self, other):
+         return self.__str__() > other.__str__()
